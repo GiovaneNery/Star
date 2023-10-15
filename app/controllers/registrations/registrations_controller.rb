1bb9e90afca0a622022e8d4c5da091c4d7e18a04
Giovane Nery <giovane.nery@hotmail.com>
2023-10-15 17:14:06 +0200

    Fixed

--- app/controllers/registrations/registrations_controller.rb
+++ app/controllers/registrations/registrations_controller.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
 class Registrations::RegistrationsController < Devise::RegistrationsController
-  def create
-    super do |resource|
-      if params[:user][:photo].present?
-        upload = Cloudinary::Uploader.upload(params[:user][:photo])
-        resource.update(photo: upload['secure_url'])
-      end
-    end
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :photo)
   end
 end
--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,5 +1,5 @@
 Cloudinary.config do |config|
-  config.cloud_name = 'Star'
+  # config.cloud_name = 'Star'
   config.api_key = '668448312176542'
   config.api_secret = 'DEfcWxgOtyKHTfOzdy4Vpks7Q3Y'
   config.secure = true
