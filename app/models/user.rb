61fbf952821f804fdf802cb4e9f547ecd574ddfe
Giovane Nery <giovane.nery@hotmail.com>
2023-10-17 20:15:58 +0200

    Added 3 roles to registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one_attached :photo
+  validates :role, presence: true
+
+  enum role: { Star: 'Star', Angel: 'Angel', Company: 'Company' }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
